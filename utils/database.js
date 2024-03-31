5aae4e3dedcbac69a6290d70d9c0c47e75565357
AbhishekNSanthosh <abhisheksanthoshofficial19@gmail.com>
2024-03-31 08:34:03 +0530

    refactor:mongodb-connection

--- utils/database.js
+++ utils/database.js
@@ -1,16 +1,26 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
 
+const uri = process.env.MONGODB_URL; 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 module.exports = connectToDB = async () => {
-  mongoose.set("strictQuery", true);
+  // mongoose.set("strictQuery", true);
   try {
-    await mongoose.connect(process.env.MANGODB_URL, {
-      dbName: "sparkz24",
-    });
-    isConnected = true;
-    console.log("Database Sucessfully connected");
-  } catch (error) {
-    console.log(error);
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 };
